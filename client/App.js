8c6657593847ba2b72bef9e52cbe8303abb4d5ce
FarDean <sfh1212@gmail.com>
2020-12-24 10:31:46 +0330

    before react-responseive

--- client/App.js
+++ client/App.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { BrowserRouter } from "react-router-dom";
 import { GlobalProvider } from './context/GlobalContext';
 import MainRouter from './MainRouter';
-
+import 'antd/dist/antd.css';
+import './assets/css/style.css'
 export default function App() {
     return (
         <GlobalProvider>
--- client/components/Home.js
+++ client/components/Home.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React,{useState,useEffect,useContext} from 'react'
 import { Link } from "react-router-dom";
+import { Layout } from 'antd';
+import { Typography } from 'antd';
+import { Row, Col, Space } from 'antd';
+
+import { MenuUnfoldOutlined,MenuFoldOutlined } from '@ant-design/icons'
+
+const { Header } = Layout
+const { Title } = Typography;
 
 export default function Home() {
+    const [burgerOpen, setBurgerOpen] = useState(false)
+
     return (
-        <div>
-            Hi mother fucker
-            <Link to='/signin'>Sign In</Link>
-        </div>
+        <Layout>
+            <Header>
+                <Row justify='space-between' align='middle'>
+                    <Col>
+                        <Title type="warning" level={3}>Hi mother fucker</Title>
+                    </Col>
+                    
+                    <Col className='hide'>
+                        <Space size={25}>
+                            <Link to='/signin'><Typography.Link level={4}>kos</Typography.Link></Link>
+                            <Typography.Link>Sign In</Typography.Link>
+                            <Link to='/signin'><Typography.Link>Sign In</Typography.Link></Link>
+                            <Link to='/signin'><Typography.Link>Sign In</Typography.Link></Link>
+                        </Space>
+                    </Col>
+                    {burgerOpen ? <MenuFoldOutlined style /> : <MenuUnfoldOutlined style={{fontSize:'25px',color: 'lime'}} />}
+                    
+                </Row>
+            </Header>
+        </Layout>
     )
 }
