bca312d3d5221634de6c216602147a2f8eb2a97a
FarDean <sfh1212@gmail.com>
2021-02-25 08:42:18 +0330

    tried testing but failed

---
+++ client/tests/profile.test.js
@@ -0,0 +1,8 @@
+// import { authenticated } from "./../helpers/api-auth";
+// import Profile from './../components/user/Profile'
+// import renderer from 'react-test-renderer'
+// import { MemoryRouter } from "react-router-dom";
+
+// jest.mock("./../helpers/api-auth")
+
+// const dummyProfile =
