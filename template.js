c0fcb065c4562304481ed5cb9534ecc6e5c4cea0
FarDean <sfh1212@gmail.com>
2021-05-14 16:22:00 +0430

    changed fav icon

--- template.js
+++ template.js
@@ -4,7 +4,7 @@ export default function Template({ markup, helmet }) {
     <html lang="en" ${helmet.htmlAttributes.toString()}>
     <head>
         <meta charset="UTF-8">
-
+        <link rel="icon" type="image/png" href="https://img.icons8.com/cute-clipart/50/000000/book.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         ${helmet.title.toString()}
         ${helmet.meta.toString()}
